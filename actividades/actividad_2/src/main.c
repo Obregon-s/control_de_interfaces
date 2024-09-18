63c9cef6b6d26f6fb2b1c514a8b88ddf55f3a6f0
Obregon-s <163901310+Obregon-s@users.noreply.github.com>
2024-09-18 09:07:44 -0300

    Update main.c
    Actividad conclusa

--- actividades/actividad_2/src/main.c
+++ actividades/actividad_2/src/main.c
@@ -9,6 +9,8 @@ int main(void) {
     stdio_init_all();
 	// Demora para esperar la conexion
 	sleep_ms(1000);
+	int16_t ab;
+	int16_t ba;
 	// Cuanto ocupa cada tipo de dato
 	printf("Un char ocupa %d byte\n", sizeof(char));
 	printf("Un short ocupa %d bytes\n", sizeof(short));
@@ -18,6 +20,13 @@ int main(void) {
 	printf("Un double ocupa %d bytes\n", sizeof(double));
 
     while (true) {
+		printf ("  Dame un numero:\n");
+		scanf ("%hd",&ab);9
+		fflush (stdin);
+		printf ("  Dame otro numero:\n");
+		scanf ("%hd",&ba)
+		fflush (stdin);
+		printf("  El resultado es %hd\n", ab+ba);
 
     }
     return 0;
